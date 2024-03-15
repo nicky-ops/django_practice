e0c7bf652f9414a36c51ff5a599a4295a28147b7
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-03-15 13:02:25 +0300

    delete an application

--- weblog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Category(models.Model):
-    title = models.CharField(max_length=250)
-    slug = models.CharField(unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
