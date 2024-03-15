d286646bdb7e46ab5e47812e53bf57fcf6180e95
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-03-15 12:57:21 +0300

    registeer models in the admin interface

---
+++ cms/weblog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Entry
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug': ['title']}
+
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Category, CategoryAdmin)
