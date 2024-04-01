1aa04e2448cfb9151f3ee0f4791a9e3ebec67477
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-01 22:59:26 +0300

    new changes

--- cms/weblog/urls.py
+++ cms/weblog/urls.py
@@ -1,19 +1,9 @@
 from django.urls import path, include
-from .models import Entry
-from . import views
-from .views import EntryDetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView
+from weblog import views
 
 
-entry_info_dict = {
-    'queryset': Entry.objects.all(),
-    'date_field': 'pub_date',
-}
 urlpatterns = [
     path('weblog/', views.entries_index, name="entries_index"),
-    path('weblog/<int:year>/<str:month>/<int:day>/<slug:slug>/', EntryDetailView.as_view(), name='entry-detail'),
-    path('weblog/', ArchiveIndexView.as_view(**entry_info_dict), name='archive_index'),
-    path('weblog/<int:year>/', YearArchiveView.as_view(**entry_info_dict), name='archive_year'),
-    path('weblog/<int:year>/<str:month>/', MonthArchiveView.as_view(**entry_info_dict), name='archive_month'),
-    path('weblog/<int:year>/<str:month>/<int:day>/', DayArchiveView.as_view(**entry_info_dict), name='archive_day'),
-    path('weblog/<int:year>/<str:month>/<int:day>/<slug:slug>/', DateDetailView.as_view(model=Entry, date_field='pub_date'), name='entry_detail'),
+    path('weblog/<int:year>/<str:month>/<int:day>/<slug:slug>/', views.entry_detail, name='entry_detail')
+
 ]
