fbb6915180ca064a6b32f0ad751ea4b8248712d5
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-02 10:02:14 +0300

    remove a project

--- cms/weblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from weblog import views
-
-
-urlpatterns = [
-    path('weblog/', views.entries_index, name="entries_index"),
-    path('weblog/<int:year>/<str:month>/<int:day>/<slug:slug>/', views.entry_detail, name='entry_detail')
-
-]
