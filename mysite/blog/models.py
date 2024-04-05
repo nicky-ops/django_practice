bd467fb00484c0614ac17434ea8ab882e8af9655
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-05 23:40:46 +0300

    SEO friendly urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+# Model Manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+        .filter(status=Post.Status.PUBLISHED)
 # Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, 
                 on_delete=models.CASCADE,
                 related_name='blog_posts')
@@ -17,6 +23,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = [
@@ -28,3 +37,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
