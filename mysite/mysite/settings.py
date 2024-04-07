764c138ac7ea50d7b747933a8347df93b9b7dc98
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-07 20:31:36 +0300

    sSMTP configuration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nickyrutto@gmail.com'
+EMAIL_HOST_PASSWORD = 'usip qrmz wiam twca'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Application definition
 
 INSTALLED_APPS = [
