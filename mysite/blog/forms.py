08f3388469ffc9621878823f29d5067bfec07aaf
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-08 23:23:48 +0300

    email form

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
-    email = forms.EmailField()
+    From = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
