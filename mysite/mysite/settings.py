916751fed2676340c30edef7c1abef905b5a5869
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-13 15:32:21 +0300

    registeer new apps in list of installed apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,6 +35,8 @@ EMAIL_HOST_PASSWORD = 'usip qrmz wiam twca'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,6 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
