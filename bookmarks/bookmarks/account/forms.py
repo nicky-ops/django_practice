3862cc13c6018aab3e26f7ff8d3a1ea6ddad57ec
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-18 23:28:53 +0300

    preventing users from using an existing email to sign up

--- bookmarks/bookmarks/account/forms.py
+++ bookmarks/bookmarks/account/forms.py
@@ -34,3 +34,9 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
