5b0e8d3a1e551dff552d9228b00b1452a466df0b
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-19 15:53:03 +0300

    sadd google authentication to list of authentication backends

--- bookmarks/bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/bookmarks/settings.py
@@ -42,7 +42,8 @@ AUTHENTICATION_BACKENDS =[
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
     'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth', 
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 # Application definition
