4e6fe5611c336eff3998727d22d41c3360168821
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-02 18:49:19 +0300

    add images app to list of installed apps

--- bookmarks/bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/bookmarks/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "social_django",
-    "django_extensions"
+    "django_extensions",
+    "images.apps.ImagesConfig",
 ]
 
 MIDDLEWARE = [
