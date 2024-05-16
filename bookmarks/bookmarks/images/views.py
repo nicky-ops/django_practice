abc22641fe3e455b4ebcc6543a512866d60e1862
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-16 19:25:50 +0300

    image list vpaginated view

--- bookmarks/bookmarks/images/views.py
+++ bookmarks/bookmarks/images/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 @login_required
@@ -53,3 +55,25 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # if AJAX request and page out of range return an empty page
+            return HttpResponse('')
+        #  If page is out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
