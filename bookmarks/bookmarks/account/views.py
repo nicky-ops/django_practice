5c7b9d3729d777438b3f9cedbea340d5fea2bf7a
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-20 17:14:25 +0300

    adding a create action function to create new actions to the activity stream

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
 
 # Create your views here.
 @login_required
@@ -50,6 +51,7 @@ def register(request):
             # Save the User object
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {
                 'new_user':new_user}
             )
@@ -110,6 +112,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create( user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status':'ok'})
