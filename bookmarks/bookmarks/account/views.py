6b27e54b3cc0c3a61ae355b0891780039ded11c8
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-20 10:40:28 +0300

    user list and detail views

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -6,6 +6,8 @@ from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditFor
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required
@@ -75,3 +77,21 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html', {
+        'section': 'people',
+        'users': users
+    })
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {
+        'section': 'people',
+        'user': user
+    })
