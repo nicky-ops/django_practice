f0345073fb55c0ea22b21753bffae227c8f4ad18
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-21 10:18:50 +0300

    adding select related and prefetch related methods to the query

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -12,6 +12,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
 from actions.utils import create_action
+from actions.models import Action
 
 # Create your views here.
 @login_required
@@ -81,7 +82,14 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # if user is following others, retrive only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions':actions})
 
 
 @login_required
