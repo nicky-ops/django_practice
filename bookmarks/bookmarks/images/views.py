f19382da5ae3432258d9d4a33bccdcc897a8f0fc
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-21 17:47:48 +0300

    establish the Redis connection and incrementing the value of a key by 1.

--- bookmarks/bookmarks/images/views.py
+++ bookmarks/bookmarks/images/views.py
@@ -9,6 +9,12 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+
+# Connect to redis
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 # Create your views here.
 @login_required
@@ -35,10 +41,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image, 'total_views': total_views})
 
 
 @login_required
