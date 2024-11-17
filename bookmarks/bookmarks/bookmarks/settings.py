7e17e76873601b26b13d64a00cf560d0461f844a
Nickson K Rop <84801830+nicky-ops@users.noreply.github.com>
2024-11-17 21:39:07 +0300

    Update settings.py
    import redis

--- bookmarks/bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/bookmarks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import redis
 from urllib.parse import urlparse
 from pathlib import Path
 from django.urls import reverse_lazy
