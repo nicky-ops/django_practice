7e5d569ccea60a727d16b4f58a93c2eb36e3c774
Nickson K Rop <84801830+nicky-ops@users.noreply.github.com>
2024-11-17 21:15:15 +0300

    Update bookmarklet.js

--- bookmarks/bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/bookmarks/images/static/js/bookmarklet.js
@@ -1,4 +1,4 @@
-const siteUrl = '//social-bookmarking-website.onrender.com';
+const siteUrl = '//social-bookmarking-website.onrender.com/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
